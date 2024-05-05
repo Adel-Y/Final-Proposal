c25c6d03a682b2b5d84b72ddc6b1072b8bda940e
Adel <yassinadel@hotmail.com>
2024-05-05 16:38:24 +0200

    basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+
+
+function DraggableItem(){
+
+  const [dragging, setDragging] = useState(false);
+
+  function handleDragStart(e) {
+    e.dataTransfer.setData('text/plain', e.target.id);
+    setDragging(true);
+  };
+
+  function handleDragOver(e) {
+    e.preventDefault();
+  };
+
+  function handleDrop(e){
+    e.preventDefault();
+    const data = e.dataTransfer.getData('text/plain');
+    const draggedItem = document.getElementById(data);
+    const dropZone = e.target;
+    dropZone.appendChild(draggedItem);
+    setDragging(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+             <div
+              id="drag-item"
+              draggable
+              onDragStart={handleDragStart}
+
+            >
+              Drag Me
+            </div>
+            
+    </>
   );
+
 }
 
+function DropZone(){
+
+  const [dragging, setDragging] = useState(false);
+  function handleDragStart(e) {
+    e.dataTransfer.setData('text/plain', e.target.id);
+    setDragging(true);
+  };
+
+  function handleDragOver(e) {
+    e.preventDefault();
+  };
+
+  function handleDrop(e){
+    e.preventDefault();
+    const data = e.dataTransfer.getData('text/plain');
+    const draggedItem = document.getElementById(data);
+    const dropZone = e.target;
+    dropZone.appendChild(draggedItem);
+    setDragging(true);
+  };
+
+  return (
+    <>
+
+
+          <div className='zone'>
+            <div
+              className={'drop-zone'}
+              onDragOver={handleDragOver}
+              onDrop={handleDrop}
+            >
+              Drop Here
+            </div>
+
+          </div>
+
+
+    </>
+  );
+
+}
+
+
+
+ function App(){
+  return(
+  <>
+    <div className='page'>
+
+          <div className='pallet'>
+
+          <DraggableItem/>
+          <DraggableItem/>
+
+          </div>
+
+          <br></br>
+
+
+          <div className='zone'>
+
+              <DropZone/>
+
+          </div>
+
+    </div>
+    </>
+
+);
+
+ }
+
 export default App;
+
+
