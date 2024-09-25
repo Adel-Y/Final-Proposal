1d532f628e290f7698b10df68dbef7d2f9f80d9b
Adel <yassinadel@hotmail.com>
2024-09-25 20:39:11 +0200

    was able to extract tables from the database eg. entity with its attributes

--- backend/routes/queryRoutes.js
+++ backend/routes/queryRoutes.js
@@ -1,5 +1,6 @@
 console.log('I entered')
 const express = require('express');
+const { stringify, parse } = require('flatted');
 const router = express.Router();
 const Node = require('../models/Node');
 const Edge = require('../models/Edge');
@@ -8,6 +9,7 @@ const Edge = require('../models/Edge');
 router.get('/testerQuery', async (req, res) => {
     try {
       const relationshipNodes = await Node.where("type").equals("Relationship");
+      //console.log(relationshipNodes)
 
       const relationshipEdges = await Edge.where("type").equals("custom-edge");
 
@@ -27,10 +29,39 @@ router.get('/testerQuery', async (req, res) => {
             return rel;
         })
       
-      console.log(relationships)
+    //   console.log(relationships)
+
+    const attributeNodes = await Node.where("type").equals("Attribute");
+
+    const entityNodes= await Node.where("type").equals("Entity");
+
+    const attributesEdges = await Edge.where("type").equals("straight-edge");
+
+    const tables =entityNodes.map((node)=>{
+
+        let tab ={id:node.id,name:node.data.name,attributes:[]};
+
+        attributesEdges.map((edge)=>{
+                if(node.id === edge.target){
+
+                    tab.attributes.push(attributeNodes.map((node)=>{
+                        if(node.id===edge.source){
+                            return {
+                                name:node.data.name, 
+                                primaryKey:node.data.primaryKey,
+                                type:node.data.attributeType,
+                                dataFormat:node.data.dataType,
+                                size:node.data.dataSize}
+                        }
+                    }).filter((node)=>node)) 
+                }
+
+            })
+            return tab;
+        })
 
 
-      res.send(relationships);
+      res.send(tables);
     } catch (err) {
       res.status(500).send(err.message);
     }
