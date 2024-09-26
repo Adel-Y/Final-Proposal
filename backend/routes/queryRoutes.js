f5702da51d01836df19c66959493fba43b1e8896
Adel <yassinadel@hotmail.com>
2024-09-26 21:13:10 +0200

    successfully transformed to relational schema, but remains display of tables

--- backend/routes/queryRoutes.js
+++ backend/routes/queryRoutes.js
@@ -1,9 +1,137 @@
 console.log('I entered')
 const express = require('express');
-const { stringify, parse } = require('flatted');
 const router = express.Router();
 const Node = require('../models/Node');
 const Edge = require('../models/Edge');
+
+
+
+
+const cardinalityRenderer =(value)=>{
+
+    let x ="";
+    
+    switch(value){
+        
+        case'zero-to-one':{
+
+            x ="one"
+            return x;
+
+        }
+        case'one-to-one':{
+            x ="one"
+            return x;
+           
+        }
+        case'zero-to-many':{
+            x="many"
+            return x;
+           
+        }
+        case'one-to-many':{
+            x="many"
+            return x;
+        }
+
+    }
+
+
+}
+
+function flipCoin() {
+    const randomIndex = Math.floor(Math.random() * 2); // Randomly returns 0 or 1
+    return randomIndex === 0 ? 0 : 1;
+  }
+
+const tablesRenderer = (relationships,tables)=>{
+
+    const relationalModels = relationships.map((rel)=>{
+
+        if(rel.cardinality1 ==="one" && rel.cardinality2==="many"){
+
+            const foreignKey= tables.filter((table)=>table.id===rel.entity2)[0].name+"_id"
+
+            const target=tables.filter((table)=>table.id===rel.entity1 )
+            target[0].attributes.push({foreignKey:foreignKey})
+            const model ={name:target[0].name,id:'id'}
+            let i =0;
+            target[0].attributes.map((att)=>{
+                model["attribute"+i]=att
+                i++
+            })
+
+           return model
+        }
+
+        if(rel.cardinality1 ==="many" && rel.cardinality2==="one"){
+
+            const foreignKey= tables.filter((table)=>table.id===rel.entity1)[0].name+"_id"
+
+             const target=tables.filter((table)=>table.id===rel.entity2 )
+             target[0].attributes.push({foreignKey:foreignKey})
+             const model ={name:target[0].name,id:'id'}
+             let i =0;
+             target[0].attributes.map((att)=>{
+                 model["attribute"+i]=att
+                 i++
+             })
+            return model
+        }
+
+
+        if(rel.cardinality1 ==="many" && rel.cardinality2==="many"){
+
+            const foreignKey1= tables.filter((table)=>table.id===rel.entity1)[0].name+"_id"
+
+            const foreignKey2= tables.filter((table)=>table.id===rel.entity2)[0].name+"_id"
+
+             const model={name:rel.name,id:"id",key1:foreignKey1,key2:foreignKey2}
+            return model
+        }
+        if(rel.cardinality1 ==="one" && rel.cardinality2==="one"){
+
+            const flip = flipCoin();
+
+            if(flip===0){
+
+                const foreignKey= tables.filter((table)=>table.id===rel.entity1)[0].name+"_id"
+
+                const target=tables.filter((table)=>table.id===rel.entity2 )
+                target[0].attributes.push({foreignKey:foreignKey})
+                const model ={name:target[0].name,id:'id'}
+                let i =0;
+                target[0].attributes.map((att)=>{
+                    model["attribute"+i]=att
+                    i++
+                })
+               return model
+
+            }
+            
+            else if(flip===1){
+
+
+                const foreignKey= tables.filter((table)=>table.id===rel.entity2)[0].name+"_id"
+
+                const target=tables.filter((table)=>table.id===rel.entity1 )
+                target[0].attributes.push({foreignKey:foreignKey})
+                const model ={name:target[0].name,id:'id'}
+                let i =0;
+                target[0].attributes.map((att)=>{
+                    model["attribute"+i]=att
+                    i++
+                })
+               return model
+            }
+
+
+        }
+    })
+
+    return relationalModels
+
+}
 
 
 router.get('/testerQuery', async (req, res) => {
@@ -13,15 +141,21 @@ router.get('/testerQuery', async (req, res) => {
 
       const relationshipEdges = await Edge.where("type").equals("custom-edge");
 
+      const attributeNodes = await Node.where("type").equals("Attribute");
+
+      const entityNodes= await Node.where("type").equals("Entity");
+  
+      const attributesEdges = await Edge.where("type").equals("straight-edge");
+
     const relationships =relationshipNodes.map((node)=>{
         // console.log(node)
-        let rel ={id:node.id};
+        let rel ={id:node.id,name:node.data.name};
         let i=1;
         relationshipEdges.map((edge)=>{
                 // console.log(edge)
                 if(node.id === edge.target){
                     rel['entity'+i]=edge.source
-                    rel['cardinality'+i]=edge.data.cardinality
+                    rel['cardinality'+i]=  cardinalityRenderer(edge.data.cardinality)
                     i++;
                 }
 
@@ -31,11 +165,7 @@ router.get('/testerQuery', async (req, res) => {
       
     //   console.log(relationships)
 
-    const attributeNodes = await Node.where("type").equals("Attribute");
 
-    const entityNodes= await Node.where("type").equals("Entity");
-
-    const attributesEdges = await Edge.where("type").equals("straight-edge");
 
     const tables =entityNodes.map((node)=>{
 
@@ -51,7 +181,8 @@ router.get('/testerQuery', async (req, res) => {
                                 primaryKey:node.data.primaryKey,
                                 type:node.data.attributeType,
                                 dataFormat:node.data.dataType,
-                                size:node.data.dataSize}
+                                size:node.data.dataSize
+                            }
                         }
                     }).filter((node)=>node)) 
                 }
@@ -61,17 +192,18 @@ router.get('/testerQuery', async (req, res) => {
         })
 
 
-      res.send(tables);
+    const result =tablesRenderer(relationships,tables)
+
+
+      res.send(result);
     } catch (err) {
       res.status(500).send(err.message);
     }
   });
 
   router.get('/testerQuery2', async (req, res) => {
+    
     try {
-
-
-
 
       const relationships = await Edge.aggregate([
         {   $match:{ 
